253994121baf062a8f3fcbe3bb66c79abe7e8edf
Joshua Allen <joshutallen99@gmail.com>
2022-12-02 08:36:52 -0700

    last changes

--- INTEX/settings.py
+++ INTEX/settings.py
@@ -13,16 +13,20 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from getpass import getpass
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+l$!5x5jtb4+%r%cftd$h)th$2x%*#svkkw8yyire$*&-@e=23'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
